92f5a16d1280aba0ce8f9d45516955643bdfa236
Alan Maciel <alan.maciel.salcedo@gmail.com>
2025-09-30 19:12:22 -0600

    First commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b99ff6bfa13f0273ff8d0d0fd17cc44fab71dfdc293c7a8528280e690f084ef0" "452068f2985179294c73c5964c730a10e62164deed004a8ab68a5d778a2581da" "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4" "dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec" "0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe" "2f7fa7a92119d9ed63703d12723937e8ba87b6f3876c33d237619ccbd60c96b9" "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d" "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" "a368631abdadffb6882f9994637d7216167912311447f1ec02f9dc58e9cc62a9" default))
+ '(package-selected-packages '(eat claude-shell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
